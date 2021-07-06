407f68d5f2b70d31c3ccaeb1028dc26d67991cde
BuildTools <sachingorkarcj72019@gmail.com>
2021-07-06 11:17:55 +0530

    fixed confetti creepers not working in liquids

--- src/main/java/com/github/sachin/tweakin/confetticreepers/ConfettiCreepers.java
+++ src/main/java/com/github/sachin/tweakin/confetticreepers/ConfettiCreepers.java
@@ -7,6 +7,7 @@ import com.github.sachin.tweakin.BaseTweak;
 import com.github.sachin.tweakin.Tweakin;
 
 import org.bukkit.Color;
+import org.bukkit.EntityEffect;
 import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.entity.Creeper;
@@ -18,11 +19,30 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.inventory.meta.FireworkEffectMeta;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class ConfettiCreepers extends BaseTweak implements Listener{
 
+    private FireworkEffect.Builder builder;
+
     public ConfettiCreepers(Tweakin plugin) {
         super(plugin, "confetti-creepers");
+    }
+
+    @Override
+    public void reload() {
+        super.reload();
+        this.builder = FireworkEffect.builder();
+        builder.with(Type.BURST);
+        List<Integer> colors = getConfig().getIntegerList("colors");
+        List<Color> fireWorkColors = new ArrayList<>();
+        if(!colors.isEmpty()){
+            for (Integer color : colors) {
+                fireWorkColors.add(Color.fromRGB(color));
+            }
+            builder.flicker(true);
+            builder.withColor(fireWorkColors);
+        }
     }
 
 
@@ -36,20 +56,11 @@ public class ConfettiCreepers extends BaseTweak implements Listener{
             e.setCancelled(true);
             Firework firework = creeper.getWorld().spawn(creeper.getLocation().clone().subtract(0,0.2,0), Firework.class);
             FireworkMeta meta = firework.getFireworkMeta();
-            FireworkEffect.Builder builder = FireworkEffect.builder();
-            builder.with(Type.BURST);
-            List<Integer> colors = getConfig().getIntegerList("colors");
-            List<Color> fireWorkColors = new ArrayList<>();
-            if(!colors.isEmpty()){
-                for (Integer color : colors) {
-                    fireWorkColors.add(Color.fromRGB(color));
-                }
-                builder.flicker(true);
-                
-                builder.withColor(fireWorkColors);
-                meta.addEffect(builder.build());
-                firework.setFireworkMeta(meta);
-            }
+            meta.addEffect(builder.build());
+            firework.setFireworkMeta(meta);
+            new BukkitRunnable(){
+                public void run() {firework.detonate();};
+            }.runTaskLater(getPlugin(), 2);
             creeper.remove();
 
         }
