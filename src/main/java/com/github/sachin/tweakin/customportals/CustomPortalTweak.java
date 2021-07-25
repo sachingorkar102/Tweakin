12e3adbb919f95c581affd79e9d1e5ccbfefb4ad
CatAndPaste <cnoiseomni@gmail.com>
2021-07-25 15:28:07 +0300

    Custom Portal update - Recursive algorithm to find all the valid portal blocks - Algorithm stops if block of not validPortalMaterial or Air (Fire) found, or portal size in any dimension exceeds max values from the config file - Now portal can be lit from any direction and any block side - Added "max-width" and "max-height" options to the custom_portal config section - Added "debug" option to the custom_portal config section to show more information in the console

--- src/main/java/com/github/sachin/tweakin/customportals/CustomPortalTweak.java
+++ src/main/java/com/github/sachin/tweakin/customportals/CustomPortalTweak.java
@@ -1,20 +1,13 @@
 package com.github.sachin.tweakin.customportals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
+import java.text.MessageFormat;
+import java.util.*;
 
 import com.github.sachin.tweakin.BaseTweak;
 import com.github.sachin.tweakin.Tweakin;
 
 import org.bukkit.Axis;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World.Environment;
@@ -32,17 +25,75 @@ import org.bukkit.inventory.EquipmentSlot;
 
 
 // permission: tweakin.customportal.use
-public class CustomPortalTweak extends BaseTweak implements Listener{
-
-    private List<Biome> endBiomes = new ArrayList<>();
+public class CustomPortalTweak extends BaseTweak implements Listener {
+    private final Tweakin plugin;
+    private final List<Biome> endBiomes;
 
     public CustomPortalTweak(Tweakin plugin) {
         super(plugin, "custom-portal");
+        this.plugin = plugin;
         this.endBiomes = Arrays.asList(Biome.END_BARRENS,Biome.END_HIGHLANDS,Biome.END_MIDLANDS,Biome.THE_END,Biome.SMALL_END_ISLANDS);
     }
 
+    // A little helper class which contains info about portal boundaries and does all the work to check if we exceed these.
+    private class PortalBounds {
+        int minX;
+        int maxX;
+        int minY;
+        int maxY;
 
-    private List<Material> getValidPortalBlocks(){
+        boolean facingEast;
+
+        int maxWidth;
+        int maxHeight;
+        int maxSize;
+
+        public PortalBounds(Location loc, boolean facingEast) {
+            this.maxWidth = getConfig().getInt("max-width", 20);
+            this.maxHeight = getConfig().getInt("max-width", 40);
+            this.maxSize = this.maxWidth * this.maxHeight;
+
+            this.facingEast = facingEast;
+
+            this.minX = facingEast ? loc.getBlockZ() : loc.getBlockX();
+            this.maxX = facingEast ? loc.getBlockZ() : loc.getBlockX();
+            this.minY = loc.getBlockY();
+            this.maxY = loc.getBlockY();
+        }
+
+        public boolean updateBounds(Location loc) {
+            boolean valueChanged = false;
+            int curX = facingEast ? loc.getBlockZ() : loc.getBlockX();
+            int curY = loc.getBlockY();
+            if (curX < minX) {
+                minX = curX;
+                valueChanged = true;
+            }
+            if (curX >= maxX) {
+                maxX = curX;
+                valueChanged = true;
+            }
+            if (curY < minY) {
+                minY = curY;
+                valueChanged = true;
+            }
+            if (curY >= maxY) {
+                maxY = curY;
+                valueChanged = true;
+            }
+            return valueChanged;
+        }
+
+        public boolean withinBounds() {
+            if ((maxX - minX) > maxWidth)
+                return false;
+            if ((maxY - minY) > maxHeight)
+                return false;
+            return true;
+        }
+    }
+
+    private List<Material> getValidPortalMaterials(){
         List<String> names = getConfig().getStringList("valid-portal-blocks");
         List<Material> mats = new ArrayList<>();
         for (String string : names) {
@@ -56,181 +107,87 @@ public class CustomPortalTweak extends BaseTweak implements Listener{
     @EventHandler
     public void netherPortalLitEvent(PlayerInteractEvent e){
         if(getBlackListWorlds().contains(e.getPlayer().getWorld().getName())) return;
-        
         if(e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if(e.getHand() != EquipmentSlot.HAND) return;
         if(e.getItem() == null) return;
         if(e.getClickedBlock() == null) return;
-        Player player = e.getPlayer();
-        Block clickedBlock = e.getClickedBlock();
-        if(player.getWorld().getEnvironment() == Environment.THE_END) return;
         if(e.getItem().getType() != Material.FLINT_AND_STEEL) return;
-        if(!getValidPortalBlocks().contains(e.getClickedBlock().getType())) return;
-        if(e.getBlockFace() != BlockFace.UP) return;
-        boolean facingEast = player.getFacing() == BlockFace.EAST || player.getFacing() == BlockFace.WEST;
+        Player player = e.getPlayer();
+        if(player.getWorld().getEnvironment() == Environment.THE_END) return;
         if(!player.hasPermission("tweakin.customportal.use")) return;
-        
-        List<Block> bs = getFrame(clickedBlock, clickedBlock, new ArrayList<>(),facingEast,0); 
-        
-        if(bs != null){
+        Block clickedBlock = e.getClickedBlock();
+        List<Material> validPortalMaterials = getValidPortalMaterials();
+        if(!validPortalMaterials.contains(e.getClickedBlock().getType())) return;
+        if(clickedBlock.getRelative(e.getBlockFace()).getType() != Material.AIR) return;
+
+        boolean facingEast = player.getFacing() == BlockFace.EAST || player.getFacing() == BlockFace.WEST;
+
+        Set<Block> checked = new HashSet<>();
+        BlockFace[] faces = getRelativeBlockFaces(facingEast);
+
+        Block block = clickedBlock.getRelative(e.getBlockFace());
+        PortalBounds bounds = new PortalBounds(block.getLocation(), facingEast);
+
+        if (checkBlock(block, validPortalMaterials, checked, faces, bounds)) {
+            if (getConfig().getBoolean("debug", true))
+                plugin.getLogger().info("Check completed! Found " + checked.size() + " valid portal blocks!");
             e.setCancelled(true);
-            if(!bs.contains(clickedBlock)){
-                bs.add(clickedBlock);
-            }
-            List<Block> airBlocks = getNearbyBlocks(clickedBlock.getLocation(),getConfig().getInt("radius",10), facingEast);
-
-            for (Block block : airBlocks) {
-                
-                List<Block> adjObs = new ArrayList<>();
-                Location loc = block.getLocation();
-                boolean aUp = false;
-                boolean aDown = false;
-                boolean aLeft = false;
-                boolean aRight = false;
-                int airX = facingEast ? block.getLocation().getBlockZ() : block.getLocation().getBlockX();
-                for(Block o : bs){
-                    int oX = facingEast ? o.getLocation().getBlockZ() : o.getLocation().getBlockX();
-                    Location oLoc = o.getLocation();
-                    if(airX == oX && !aUp && loc.getBlockY() > oLoc.getBlockY()){
-                        aUp = true;
-                        adjObs.add(o);
-                    }
-                    else if(airX == oX && !aDown && loc.getBlockY() < oLoc.getBlockY()){
-                        aDown = true;
-                        adjObs.add(o);
-                    }
-                    else if(loc.getBlockY() == oLoc.getBlockY() && !aRight && airX > oX){
-                        aRight = true;
-                        adjObs.add(o);
-                    }
-                    else if(loc.getBlockY() == oLoc.getBlockY() && !aLeft && airX < oX){
-                        aLeft = true;
-                        adjObs.add(o);
-                    }
-                }
-                if(adjObs != null){
-                    if(adjObs.size() == 4){
-                        block.setType(Material.NETHER_PORTAL);
-                        BlockData bd = block.getBlockData();
-                        Orientable orientable = (Orientable) bd;
-                        orientable.setAxis(facingEast ? Axis.Z : Axis.X);
-                        block.setBlockData(orientable);
-                    }
-                }
-                
-                
-            }
+            buildPortal(checked, facingEast);
+            return;
         }
 
-
-    }
-
-
-    private boolean hasFutureBlock(Block block,List<Block> orignalList,boolean facingEast){
-        List<BlockSides> sides = Arrays.asList(BlockSides.values());
-        // Collections.shuffle(sides);
-        for(BlockSides side: sides){
-            Block sideBlock = block.getLocation().add(facingEast ? 0 : side.getX(), side.getY(), facingEast ? side.getX() : 0).getBlock();
-            if(getValidPortalBlocks().contains(block.getType()) && !orignalList.contains(sideBlock)){
-                return true;
-            }
+        if (getConfig().getBoolean("debug", true))
+            plugin.getLogger().info("Check failed in default orientation with "
+                    + checked.size()
+                    + " blocks found, let's try another direction...");
+        facingEast = !facingEast;
+        bounds = new PortalBounds(block.getLocation(), facingEast);
+        faces = getRelativeBlockFaces(facingEast);
+        checked.clear();
+        if (checkBlock(block, validPortalMaterials, checked, faces, bounds)) {
+            if (getConfig().getBoolean("debug", true))
+                plugin.getLogger().info("Check completed! Found " + checked.size() + " valid portal blocks!");
+            e.setCancelled(true);
+            buildPortal(checked, facingEast);
+            return;
         }
-        return false;
+        if (getConfig().getBoolean("debug", true))
+            plugin.getLogger().info("Portal creation failed after checking " + checked.size() + " blocks.");
     }
 
-    private List<Block> getFrame(Block clickedBlock,Block starterBlock,List<Block> orignalList,boolean facingEast,int rCount){
-        if(rCount > 200){
-            return null;
+    private boolean checkBlock(Block block, List<Material> validPortalMaterials, Set<Block> checked, BlockFace[] faces, PortalBounds bounds) {
+        if (validPortalMaterials.contains(block.getType()) || checked.contains(block))
+            return true;
+        if (block.getType() != Material.AIR && block.getType() != Material.FIRE)
+            return false;
+        checked.add(block);
+        if (bounds.updateBounds(block.getLocation()))
+            if (!bounds.withinBounds())
+                return false;
+        if (!checkBlock(block.getRelative(faces[2]), validPortalMaterials, checked, faces, bounds) ||
+                !checkBlock(block.getRelative(faces[1]), validPortalMaterials, checked, faces, bounds) ||
+                !checkBlock(block.getRelative(faces[3]), validPortalMaterials, checked, faces, bounds) ||
+                !checkBlock(block.getRelative(faces[0]), validPortalMaterials, checked, faces, bounds)
+        )
+            return false;
+        return true;
+    }
+
+    private void buildPortal(Set<Block> portalBlocks, boolean facingEast) {
+        for (Block b : portalBlocks) {
+            b.setType(Material.NETHER_PORTAL);
+            BlockData bd = b.getBlockData();
+            Orientable orientable = (Orientable) bd;
+            orientable.setAxis(facingEast ? Axis.Z : Axis.X);
+            b.setBlockData(orientable);
         }
-        List<Material> validBlocks = getValidPortalBlocks();
-        Block sideBlock = null;
-        List<BlockSides> sides = Arrays.asList(BlockSides.values());
-        // Collections.shuffle(sides);
-        for(BlockSides side: sides){
-            Block b = starterBlock.getLocation().add(facingEast ? 0 : side.getX(), side.getY(), facingEast ? side.getX() : 0).getBlock();
-            if(!validBlocks.contains(b.getType())) continue;
-            if(orignalList.contains(b)) continue;
-            if(hasFutureBlock(b, orignalList, facingEast)){
-                sideBlock = b;
-                break;
-            }
-        }
-        if(sideBlock != null){
-            if(sideBlock.getLocation().getBlockY()-clickedBlock.getLocation().getBlockY() > getConfig().getInt("radius",10)){
-                return null;
-            }
-            boolean equalX = facingEast ? sideBlock.getLocation().getBlockZ() == clickedBlock.getLocation().getBlockZ() : sideBlock.getLocation().getBlockX() == clickedBlock.getLocation().getBlockX();
-            if(equalX && sideBlock.getLocation().getBlockY() == clickedBlock.getLocation().getBlockY() && rCount>5){
-                orignalList.add(sideBlock);
-                return orignalList;
-            }
-            if(!orignalList.contains(sideBlock)){
-                rCount++;
-                orignalList.add(sideBlock);
-                return getFrame(clickedBlock, sideBlock, orignalList, facingEast, rCount);
-            }
-            
-        
-        }
-        return null;
     }
 
-    public static List<Block> getNearbyBlocks(Location location, int radius,boolean facingEast) {
-        List<Block> blocks = new ArrayList<Block>();
-        int sx = location.getBlockX();
-        int sy = location.getBlockY();
-        int sz =location.getBlockZ();
-        for(int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
-            // if(y<sy+1) continue;
-            if(facingEast){
-                for(int z = sz - radius; z <= sz + radius; z++) {
-                   Block block = location.getWorld().getBlockAt(sx, y, z);
-                   if(block.getType() == Material.AIR){
-                       blocks.add(block);
-                   } 
-                }
-            }
-            else{
-                for(int x = sx - radius; x <= sx + radius; x++) {
-                    Block block = location.getWorld().getBlockAt(x, y, sz);
-                   if(block.getType() == Material.AIR){
-                       blocks.add(block);
-                   }
-                }
-            }
-        }
-        return blocks;
+    private BlockFace[] getRelativeBlockFaces(boolean facingEast) {
+        return new BlockFace[]{ BlockFace.UP,
+                facingEast ? BlockFace.SOUTH : BlockFace.EAST,
+                BlockFace.DOWN,
+                facingEast ? BlockFace.NORTH : BlockFace.WEST
+        };
     }
-
-    
-}
-
-enum BlockSides{
-    UP(0,1)
-    ,DOWN(0,-1)
-    ,UP_LEFT(-1,1)
-    ,UP_RIGHT(1,1)
-    ,DOWN_LEFT(-1,-1)
-    ,DOWN_RIGHT(1,-1)
-    ,MIDDLE_LEFT(1,0)
-    ,MIDDLE_RIGHT(-1,0);
-
-    private BlockSides(int x,int y){
-        this.x = x;
-        this.y = y;
-    }
-
-    private int x;
-    private int y;
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-
-
 }
