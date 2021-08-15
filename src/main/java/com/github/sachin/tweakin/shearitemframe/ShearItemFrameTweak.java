d5ca543309f784436934476a36646519bc4ee834
BuildTools <sachingorkarcj72019@gmail.com>
2021-08-15 10:29:13 +0530

    minor changes to shear-item-frame

--- src/main/java/com/github/sachin/tweakin/shearitemframe/ShearItemFrameTweak.java
+++ src/main/java/com/github/sachin/tweakin/shearitemframe/ShearItemFrameTweak.java
@@ -28,13 +28,14 @@ public class ShearItemFrameTweak extends BaseTweak implements Listener{
 
     @EventHandler(priority = EventPriority.LOWEST,ignoreCancelled = false)
     public void onShear(PlayerInteractEntityEvent e){
-        if(!(e.getRightClicked() instanceof ItemFrame) || e.isCancelled()) return;
+        if(!(e.getRightClicked() instanceof ItemFrame)) return;
         Player player = e.getPlayer();
         if(player.isSneaking() || !player.hasPermission("tweakin.shearitemframe.use")) return;
         if(getBlackListWorlds().contains(player.getWorld().getName())) return;
         ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
         if(item.getType() != Material.SHEARS) return;
         ItemFrame frame = (ItemFrame) e.getRightClicked();
+        if(frame.getPersistentDataContainer().has(key, PersistentDataType.INTEGER)) return;
         if(!frame.getItem().getType().isAir()){
             boolean placed = plugin.getNmsHelper().placeItem(player, frame.getLocation(), new ItemStack(Material.DIRT), frame.getAttachedFace(),null);
             if(placed){
