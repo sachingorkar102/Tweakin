bb78d16f04736da723b6fac3420cc48c4371d1ba
BuildTools <sachingorkarcj72019@gmail.com>
2021-08-30 11:11:52 +0530

    fixed critical dupe in right click armor

--- src/main/java/com/github/sachin/tweakin/rightclickarmor/RightClickArmor.java
+++ src/main/java/com/github/sachin/tweakin/rightclickarmor/RightClickArmor.java
@@ -62,37 +62,38 @@ public class RightClickArmor extends BaseTweak implements Listener {
     @EventHandler
     public void armorClickEvent(InventoryClickEvent e){
         if(e.getClick() != ClickType.RIGHT) return;
-        if(e.getInventory().getType() != InventoryType.CRAFTING) return;
-        Player player = (Player) e.getWhoClicked();
-        if(!player.hasPermission("tweakin.armorclick")) return;
-        if(e.getCurrentItem() == null) return;
-        ItemStack item = e.getCurrentItem().clone();
-        String itemName = item.getType().name();
-        PlayerInventory inv = player.getInventory();
-        ItemStack swapItem;
-        if(itemName.endsWith("HELMET")){
-            e.setCancelled(true);
-            swapItem = inv.getHelmet();
-            inv.setItem(EquipmentSlot.HEAD,item);
-            inv.setItem(e.getSlot(),swapItem);
-        }
-        else if(itemName.endsWith("CHESTPLATE") || itemName.endsWith("ELYTRA")){
-            e.setCancelled(true);
-            swapItem = inv.getChestplate();
-            inv.setItem(EquipmentSlot.CHEST,item);
-            inv.setItem(e.getSlot(),swapItem);
-        }
-        else if(itemName.endsWith("LEGGINGS")){
-            e.setCancelled(true);
-            swapItem = inv.getLeggings();
-            inv.setItem(EquipmentSlot.LEGS,item);
-            inv.setItem(e.getSlot(),swapItem);
-        }
-        else if(itemName.endsWith("BOOTS")){
-            e.setCancelled(true);
-            swapItem = inv.getBoots();
-            inv.setItem(EquipmentSlot.FEET,item);
-            inv.setItem(e.getSlot(),swapItem);
+        if(e.getClickedInventory() != null && e.getClickedInventory().getType()==InventoryType.PLAYER){
+            Player player = (Player) e.getWhoClicked();
+            if(!player.hasPermission("tweakin.armorclick")) return;
+            if(e.getCurrentItem() == null) return;
+            ItemStack item = e.getCurrentItem().clone();
+            String itemName = item.getType().name();
+            PlayerInventory inv = player.getInventory();
+            ItemStack swapItem;
+            if(itemName.endsWith("HELMET")){
+                e.setCancelled(true);
+                swapItem = inv.getHelmet();
+                inv.setItem(EquipmentSlot.HEAD,item);
+                inv.setItem(e.getSlot(),swapItem);
+            }
+            else if(itemName.endsWith("CHESTPLATE") || itemName.endsWith("ELYTRA")){
+                e.setCancelled(true);
+                swapItem = inv.getChestplate();
+                inv.setItem(EquipmentSlot.CHEST,item);
+                inv.setItem(e.getSlot(),swapItem);
+            }
+            else if(itemName.endsWith("LEGGINGS")){
+                e.setCancelled(true);
+                swapItem = inv.getLeggings();
+                inv.setItem(EquipmentSlot.LEGS,item);
+                inv.setItem(e.getSlot(),swapItem);
+            }
+            else if(itemName.endsWith("BOOTS")){
+                e.setCancelled(true);
+                swapItem = inv.getBoots();
+                inv.setItem(EquipmentSlot.FEET,item);
+                inv.setItem(e.getSlot(),swapItem);
+            }
         }
     }
 
