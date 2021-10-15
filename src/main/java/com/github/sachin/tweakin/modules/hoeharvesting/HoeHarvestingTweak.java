f730f546af74f784f4770f958b805cb55bcf92f4
BuildTools <sachingorkarcj72019@gmail.com>
2021-10-15 12:22:30 +0530

    hoe actually gets damaged

--- src/main/java/com/github/sachin/tweakin/modules/hoeharvesting/HoeHarvestingTweak.java
+++ src/main/java/com/github/sachin/tweakin/modules/hoeharvesting/HoeHarvestingTweak.java
@@ -1,5 +1,7 @@
 package com.github.sachin.tweakin.modules.hoeharvesting;
 
+import java.util.Random;
+
 import com.github.sachin.tweakin.BaseTweak;
 import com.github.sachin.tweakin.Tweakin;
 
@@ -8,16 +10,20 @@ import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 
 public class HoeHarvestingTweak extends BaseTweak implements Listener{
 
-    
+    private final Random RANDOM = new Random();
 
     public HoeHarvestingTweak(Tweakin plugin) {
         super(plugin, "hoe-harvesting");
@@ -40,21 +46,6 @@ public class HoeHarvestingTweak extends BaseTweak implements Listener{
         }
     }
 
-    // Not in use
-    // public boolean isHarvestAbleMaterial(Material type){
-    //     String blockName = type.toString();
-    //     for (String string : getConfig().getStringList("harvestable-materials")) {
-    //         if(string.startsWith("^")){
-    //             if(blockName.endsWith(string.replace("^", ""))){
-    //                 return true;
-    //             }
-    //         }
-    //         if(blockName.equals(string)){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
 
 
     @EventHandler
@@ -77,12 +68,44 @@ public class HoeHarvestingTweak extends BaseTweak implements Listener{
                     Material blockType = loc.getBlock().getType();
                     if(matchesHarvestable(blockType)){
                         loc.getBlock().breakNaturally(item);
-                        
-                    }
+                        damageItem(1, item, RANDOM, player);
+                    }  
                 }
             }
         }
         
+    }
+
+    private ItemStack damageItem(int amount,ItemStack item,Random random,Player player){
+        ItemMeta meta = item.getItemMeta();
+        if(!(meta instanceof Damageable) || amount < 0) return item;
+        int m = item.getEnchantmentLevel(Enchantment.DURABILITY);
+        int k = 0;
+        for (int l = 0; m > 0 && l < amount; l++) {
+            if (random.nextInt(m +1) > 0){
+                k++; 
+            }
+        }  
+        amount -= k;
+        if(player != null){
+            PlayerItemDamageEvent damageEvent = new PlayerItemDamageEvent(player, item, amount);
+            plugin.getServer().getPluginManager().callEvent(damageEvent);
+            if(amount != damageEvent.getDamage() || damageEvent.isCancelled()){
+                damageEvent.getPlayer().updateInventory();
+            }
+            else if(damageEvent.isCancelled()){
+                return item;
+            }
+            amount = damageEvent.getDamage();
+
+        }
+        if (amount <= 0)
+            return item; 
+        
+        Damageable damageable = (Damageable) meta;
+        damageable.setDamage(damageable.getDamage()+amount);
+        item.setItemMeta(meta);    
+        return item;
     }
 
     private boolean matchesHarvestable(Material mat){
