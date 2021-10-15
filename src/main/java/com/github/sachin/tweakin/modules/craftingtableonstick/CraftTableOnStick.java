7d8e1798e93e7daf7c75929c8f0b8e6403c6ed2b
BuildTools <sachingorkarcj72019@gmail.com>
2021-10-15 12:52:27 +0530

    same inventory update fix for crafting table on stick

--- src/main/java/com/github/sachin/tweakin/modules/craftingtableonstick/CraftTableOnStick.java
+++ src/main/java/com/github/sachin/tweakin/modules/craftingtableonstick/CraftTableOnStick.java
@@ -16,6 +16,7 @@ import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class CraftTableOnStick extends TweakItem implements Listener{
 
@@ -41,7 +42,12 @@ public class CraftTableOnStick extends TweakItem implements Listener{
         Player player = (Player) e.getWhoClicked();
         if(e.getCurrentItem() == null || e.getClick() != ClickType.MIDDLE) return;
         if((isSimilar(e.getCurrentItem()) || (getConfig().getBoolean("works-with-crafting-table") && e.getCurrentItem().getType()==Material.CRAFTING_TABLE)) && e.getClickedInventory() instanceof PlayerInventory && player.hasPermission(Permissions.CRATINGTABLE_ON_STICK_USE)){
-            player.openWorkbench(null, true);
+            new BukkitRunnable(){
+                @Override
+                public void run() {
+                    player.openWorkbench(null, true);
+                }
+            }.runTaskLater(plugin, 1);
         }
     }
 
