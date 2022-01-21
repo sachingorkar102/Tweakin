6b90433ce5f7e2a269503aaec78667c120a9ea4b
sandtechnology <a1294790523@hotmail.com>
2022-01-22 00:45:01 +0800

    [FastLeafDecayTweak] Remove unnecessary async feature and optimize code logic

--- core/src/main/java/com/github/sachin/tweakin/modules/fastleafdecay/FastLeafDecayTweak.java
+++ core/src/main/java/com/github/sachin/tweakin/modules/fastleafdecay/FastLeafDecayTweak.java
@@ -11,96 +11,69 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.LeavesDecayEvent;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class FastLeafDecayTweak extends BaseTweak implements Listener{
+public class FastLeafDecayTweak extends BaseTweak implements Listener {
 
     private int duration;
-    private final Set<LeavesDecayEvent> fastDecayEvents = new HashSet<>();
 
 
     public FastLeafDecayTweak(Tweakin plugin) {
         super(plugin, "fast-leaf-decay");
     }
 
+
     @Override
     public void reload() {
         super.reload();
-        this.duration = getConfig().getInt("duration",10) * 20;
+        this.duration = getConfig().getInt("duration", 10) * 20;
+        //Remove unused option
+        if (getConfig().isSet("use-async")) {
+            getConfig().set("use-async", null);
+        }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST,ignoreCancelled = true)
-    public void onLeafDecay(LeavesDecayEvent e){
-        if(fastDecayEvents.remove(e)) return;
-        if(getBlackListWorlds().contains(e.getBlock().getWorld().getName())) return;
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onLeafDecay(LeavesDecayEvent e) {
+        if (e instanceof LeavesDecayEventForChecking) return;
+        if (getBlackListWorlds().contains(e.getBlock().getWorld().getName())) return;
         e.setCancelled(true);
         Block block = e.getBlock();
-        if(getConfig().getBoolean("use-async")){
-            new BukkitRunnable(){
-                @Override
-                public void run() {
-                    List<Location> locs = getNearbyBlocks(block.getLocation(), 10,true);
-                    removeLeaves(locs);
-                }
-            }.runTaskAsynchronously(getPlugin());
-        }
-        else{
-            List<Location> locs = getNearbyBlocks(block.getLocation(), 10,false);
-            removeLeaves(locs);
-        }
-        
+        removeLeaves(block.getLocation());
+
     }
-    
-    private void removeLeaves(List<Location> locs){
-        for (Location location : locs) {
-    
-            
+
+    private void removeLeaves(Location startLocation) {
+        List<Location> leavesLocations = getNearbyLeaves(startLocation, 10);
+        for (Location location : leavesLocations) {
             Bukkit.getScheduler().scheduleSyncDelayedTask(getPlugin(), () -> {
                 Block block = location.getBlock();
-                if(block.getType().name().endsWith("LEAVES")){
-                    LeavesDecayEvent event = new LeavesDecayEvent(block);
-                    fastDecayEvents.add(event);
+                if (block.getType().name().endsWith("LEAVES")) {
+                    LeavesDecayEvent event = new LeavesDecayEventForChecking(block);
                     Bukkit.getPluginManager().callEvent(event);
-                    if(!event.isCancelled()){
+                    if (!event.isCancelled()) {
                         block.breakNaturally();
-                        locs.remove(location);
                     }
                 }
-            },new Random().nextInt(duration));
-    
-    
+            }, ThreadLocalRandom.current().nextInt(duration));
         }
     }
 
-    public List<Location> getNearbyBlocks(Location location, int radius,boolean isAsync) {
+    public List<Location> getNearbyLeaves(Location location, int radius) {
         // List<Block> blocks = new ArrayList<Block>();
         List<Location> locs = new ArrayList<>();
-        for(int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
-            for(int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
-                for(int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
+        for (int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
+            for (int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
+                for (int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
                     Block block = location.getWorld().getBlockAt(x, y, z);
-                    if(!block.getType().isAir()){
-                        if(isAsync){
-                            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, ()-> {
-                                if(isLeaf(block)){
-                                    
-                                    Leaves leaves = (Leaves) block.getBlockData();
-                                    if(!leaves.isPersistent() && leaves.getDistance() > 6 && !locs.contains(block.getLocation())){
-                                        locs.add(block.getLocation());
-                                    }
-                                }
-                            });
-                        }
-                        else{
-                            if(isLeaf(block)){
-                                    
-                                Leaves leaves = (Leaves) block.getBlockData();
-                                if(!leaves.isPersistent() && leaves.getDistance() > 6 && !locs.contains(block.getLocation())){
-                                    locs.add(block.getLocation());
-                                }
-                            }
+                    if (!block.getType().isAir() && isLeaf(block)) {
+                        Leaves leaves = (Leaves) block.getBlockData();
+                        if (!leaves.isPersistent() && leaves.getDistance() > 6 && !locs.contains(block.getLocation())) {
+                            locs.add(block.getLocation());
                         }
                     }
                 }
@@ -109,8 +82,14 @@ public class FastLeafDecayTweak extends BaseTweak implements Listener{
         return locs;
     }
 
-    private boolean isLeaf(Block block){
+    private boolean isLeaf(Block block) {
         return Tag.LEAVES.isTagged(block.getType());
     }
-    
+
+    private static class LeavesDecayEventForChecking extends LeavesDecayEvent {
+        public LeavesDecayEventForChecking(@NotNull Block block) {
+            super(block);
+        }
+    }
+
 }
