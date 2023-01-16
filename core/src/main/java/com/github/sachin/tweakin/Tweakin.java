ffe2c3ec10fc62abb03faccb192119aa57e40d63
Rothes <449181985@qq.com>
2023-01-16 22:40:06 +0800

    fix post version detect

--- core/src/main/java/com/github/sachin/tweakin/Tweakin.java
+++ core/src/main/java/com/github/sachin/tweakin/Tweakin.java
@@ -250,12 +250,12 @@ public final class Tweakin extends JavaPlugin {
     }
 
     public boolean isPost1_17(){
-        return Arrays.asList("v1_17_R1","v1_18_R1","v1_18_R2","v1_19_R1").contains(version);
+        return Arrays.asList("v1_17_R1","v1_18_R1","v1_18_R2","v1_19_R1","v1_19_R11","v1_19_R2").contains(version);
     }
 
-    public boolean is1_18() {return Arrays.asList("v1_18_R1","v1_18_R2","v1_19_R1").contains(version);}
+    public boolean is1_18() {return Arrays.asList("v1_18_R1","v1_18_R2","v1_19_R1","v1_19_R11","v1_19_R2").contains(version);}
 
-    public boolean isPost1_19() {return Arrays.asList("v1_19_R1").contains(version);}
+    public boolean isPost1_19() {return Arrays.asList("v1_19_R1","v1_19_R11","v1_19_R2").contains(version);}
 
     public List<Player> getPlacedPlayers() {
         return placedPlayers;
