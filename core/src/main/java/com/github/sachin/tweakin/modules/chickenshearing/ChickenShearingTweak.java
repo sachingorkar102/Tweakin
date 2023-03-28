b3588e921bd321b04a208b56ac0b5d2cd349c253
sachingorkar102 <sachingorkarcj72019@gmail.com>
2023-03-28 17:30:05 +0530

    added dispenser support to chicken shearing

--- core/src/main/java/com/github/sachin/tweakin/modules/chickenshearing/ChickenShearingTweak.java
+++ core/src/main/java/com/github/sachin/tweakin/modules/chickenshearing/ChickenShearingTweak.java
@@ -6,19 +6,24 @@ import com.github.sachin.tweakin.utils.Permissions;
 import com.github.sachin.tweakin.utils.TConstants;
 import com.github.sachin.tweakin.utils.annotations.Config;
 import com.github.sachin.tweakin.utils.annotations.Tweak;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.Dispenser;
+import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Chicken;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.loot.LootContext;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -34,12 +39,50 @@ public class ChickenShearingTweak extends BaseTweak implements Listener {
     @Config(key="drop-feathers-on-death")
     private boolean dropFeathersOnDeath = false;
 
+    @Config(key="dispenser-usable")
+    private boolean dispenserUsable = true;
+
     @Override
     public void reload() {
         super.reload();
         String[] damage = getConfig().getString("damage").split("-");
         minDamage = Integer.parseInt(damage[0]);
         maxDamage = Integer.parseInt(damage[1]);
+    }
+
+    @EventHandler
+    public void onDispenseItem(BlockDispenseEvent e){
+        if(e.getBlock().getType() == Material.DISPENSER && dispenserUsable){
+
+            if(e.getItem().getType()==Material.SHEARS){
+
+                Directional directional = (Directional) e.getBlock().getBlockData();
+                Block relative = e.getBlock().getRelative(directional.getFacing());
+                ItemStack shears = e.getItem();
+                for(Entity en : relative.getWorld().getNearbyEntities(relative.getLocation().add(0.5,0.5,0.5),0.5,0.5,0.5, en -> en.getType()== EntityType.CHICKEN && ((Chicken)en).isAdult() && !en.isDead())){
+                    relative.getWorld().playSound(en.getLocation(), Sound.ENTITY_SHEEP_SHEAR,1,1);
+                    if(!en.getPersistentDataContainer().has(TConstants.SHEARED_CHICKEN_KEY, PersistentDataType.INTEGER)){
+                        en.getPersistentDataContainer().set(TConstants.SHEARED_CHICKEN_KEY,PersistentDataType.INTEGER,1);
+                    }
+                    ((Chicken)en).damage(ThreadLocalRandom.current().nextInt(minDamage,maxDamage));
+                    en.getWorld().dropItem(en.getLocation(), new ItemStack(Material.FEATHER));
+                    Dispenser dispenser = (Dispenser) e.getBlock().getState();
+                    new BukkitRunnable(){
+                        @Override
+                        public void run() {
+                            ItemStack damagedShears = ItemBuilder.damageItem(1,shears.clone(),plugin.RANDOM,null);
+                            int slot = dispenser.getInventory().first(shears);
+                            if(slot != -1){
+                                dispenser.getSnapshotInventory().setItem(slot,damagedShears);
+                                dispenser.update(true);
+                            }
+                        }
+                    }.runTaskLater(plugin,1);
+                    break;
+                }
+
+            }
+        }
     }
 
 
