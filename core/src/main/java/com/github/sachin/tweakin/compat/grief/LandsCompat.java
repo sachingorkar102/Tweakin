acc41c81a4e550b6c75031ad0cc90236bd630593
sachingorkar102 <sachingorkarcj72019@gmail.com>
2023-11-24 10:47:05 +0530

    updates to LandsAPI

--- core/src/main/java/com/github/sachin/tweakin/compat/grief/LandsCompat.java
+++ core/src/main/java/com/github/sachin/tweakin/compat/grief/LandsCompat.java
@@ -1,11 +1,13 @@
 package com.github.sachin.tweakin.compat.grief;
 
+import com.github.sachin.tweakin.Tweakin;
 import com.github.sachin.tweakin.utils.TConstants;
-import me.angeschossen.lands.api.flags.Flags;
-import me.angeschossen.lands.api.integration.LandsIntegration;
+import me.angeschossen.lands.api.LandsIntegration;
+import me.angeschossen.lands.api.flags.type.Flags;
 import me.angeschossen.lands.api.land.Area;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 
 public class LandsCompat extends BaseGriefCompat{
@@ -14,13 +16,23 @@ public class LandsCompat extends BaseGriefCompat{
 
     public LandsCompat(){
         super(TConstants.LANDS);
-        this.landsIntegration = new LandsIntegration(plugin);
+        this.landsIntegration = LandsIntegration.of(Tweakin.getPlugin());
     }
     @Override
     public boolean canBuild(Player player, Location location, Material type) {
-        Area area = landsIntegration.getAreaByLoc(location);
+        Area area = landsIntegration.getArea(location);
         if(area != null){
-           return area.hasFlag(player,Flags.BLOCK_PLACE,type,true);
+           return area.hasRoleFlag(landsIntegration.getLandPlayer(player.getUniqueId()), Flags.BLOCK_PLACE,type,true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean canUseArmorStand(Player player, ArmorStand as) {
+        Area area = landsIntegration.getArea(as.getLocation());
+        if(area != null){
+
+            return area.hasRoleFlag(player.getUniqueId(),Flags.INTERACT_GENERAL);
         }
         return true;
     }
